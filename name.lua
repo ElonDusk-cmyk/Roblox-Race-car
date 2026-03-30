158bfb2c5f17c568836303a887a28b9b3446c4e8
ElonDusk-cmyk <seckertchristof@gmail.com>
2026-03-30 20:03:36 +0200

    Update name.lua

--- name.lua
+++ name.lua
@@ -733,7 +733,7 @@ UserInputService.InputChanged:Connect(function(input)
         local sliderSize = SpeedSlider.AbsoluteSize
         
         local relativeX = math.clamp((mousePos.X - sliderPos.X) / sliderSize.X, 0, 1)
-        speedValue = 0.1 + (relativeX * 999.9) -- 0.1 bis 10.0
+        speedValue = 0.1 + (relativeX * 99.9) -- 0.1 bis 10.0
         
         updateSpeed()
     end
